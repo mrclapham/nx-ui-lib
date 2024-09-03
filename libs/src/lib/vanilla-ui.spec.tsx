71eec42f358d476643684379572acd5079db3bcd
Graham Clapham <graham.clapham@mnscorp.net>
2024-09-03 22:02:56 +0100

    Add StoryBook

--- libs/src/lib/vanilla-ui.spec.tsx
+++ libs/src/lib/vanilla-ui.spec.tsx
@@ -1,6 +1,7 @@
 import { render } from '@testing-library/react';
 
 import VanillaUi from './vanilla-ui';
+import React from 'react';
 
 describe('VanillaUi', () => {
   it('should render successfully', () => {
